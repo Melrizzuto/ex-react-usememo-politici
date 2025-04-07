2ba4ef9371570c10af45efca26172e458086b620
Melrizzuto <melrizzuto4@gmail.com>
2025-04-07 10:52:30 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
-// import { useState } from 'react'
-// import axios from "axios"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [politicians, setPoliticians] = useState([])
 
-  return
+  useEffect(() => {
+    axios
+      .get("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
+      .then(response => {
+        setPoliticians(response.data)
+      })
+      .catch(error => {
+        console.error("Errore nel recupero dei dati:", error)
+      })
+  }, [])
 
+  return (
+    <div>
+      <h1>Politici</h1>
+      <div>
+        {politicians.map((politician) => (
+          <div key={politician.id}>
+            <img src={politician.image} alt={politician.name} />
+            <h2>{politician.name}</h2>
+            <h4>{politician.position}</h4>
+            <p>{politician.biography}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
