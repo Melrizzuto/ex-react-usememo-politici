c75b8cadf029c7216750a0084140cb8a09a2b1e7
Melrizzuto <melrizzuto4@gmail.com>
2025-04-07 14:21:25 +0200

    installed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,29 @@ function App() {
   useEffect(() => {
     axios
       .get("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
-      .then(response => {
+      .then((response) => {
         setPoliticians(response.data)
+        console.log(response.data)
       })
-      .catch(error => {
-        console.error("Errore nel recupero dei dati:", error)
+      .catch((error) => {
+        console.error("Richiesta dati fallita", error)
       })
   }, [])
 
   return (
-    <div>
-      <h1>Politici</h1>
-      <div>
-        {politicians.map((politician) => (
-          <div key={politician.id}>
-            <img src={politician.image} alt={politician.name} />
-            <h2>{politician.name}</h2>
-            <h4>{politician.position}</h4>
-            <p>{politician.biography}</p>
+    <div className="container my-5">
+      <h1 className="text-center mb-4">Politici</h1>
+      <div className="row gy-4">
+        {politicians.map((p) => (
+          <div className="col-sm-6 col-md-4 col-lg-3" key={p.id}>
+            <div className="card h-100 shadow-sm">
+              <img src={p.image} className="card-img-top" alt={p.name} />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{p.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{p.position}</h6>
+                <p className="card-text mt-2">{p.biography}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
